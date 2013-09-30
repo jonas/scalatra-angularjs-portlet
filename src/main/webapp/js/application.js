9297fc5a768dd135537a4b1635cc37fc3ef0dc2b
Jonas Fonseca <jonas.fonseca@gmail.com>
2013-09-29 20:09:39 -0400

    Simplify the Angular app wiring

--- src/main/webapp/js/application.js
+++ src/main/webapp/js/application.js
@@ -1,12 +1,9 @@
-angular.module('portal.services', []);
-angular.module('portal.controllers', []);
+angular.module('user-list-portlet', ['ngResource']);
 
-angular.module('portal.services').factory('Users', ['$resource', '$http', function($resource, $http) {
+angular.module('user-list-portlet').factory('Users', ['$resource', '$http', function($resource, $http) {
     return $resource("/delegate/services/users");
 }]);
 
-angular.module('portal.controllers').controller('UsersListController', ['$scope', '$location', 'Users', function($scope, $location, Users) {
-    $scope.users = Users.query();    
+angular.module('user-list-portlet').controller('UsersListController', ['$scope', '$location', 'Users', function($scope, $location, Users) {
+    $scope.users = Users.query();
 }]);
-
-angular.module('user-list-portlet', ['ngResource', 'portal.services', 'portal.controllers']);
