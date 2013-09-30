0216b6be972f70d2e86e0701cc6380d6f3dbbb5a
Jonas Fonseca <jonas.fonseca@gmail.com>
2013-09-29 23:57:56 -0400

    Move authentication code to ScentrySupport module

---
+++ src/main/scala/com/savoirfairelinux/liferay/portlet/controller/AuthenticationSupport.scala
@@ -0,0 +1,50 @@
+package com.savoirfairelinux.liferay.portlet.controller
+
+import com.liferay.portal.security.auth.CompanyThreadLocal
+import com.liferay.portal.security.auth.PrincipalThreadLocal
+import com.liferay.portal.security.permission.PermissionCheckerFactoryUtil
+import com.liferay.portal.security.permission.PermissionThreadLocal
+import com.liferay.portal.util.PortalUtil
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+import org.scalatra.auth.{ScentrySupport, ScentryConfig, ScentryStrategy}
+import org.scalatra.ScalatraBase
+import scala.util.Try
+
+case class LiferayAuthUser(id: String)
+
+class LiferayAuthStrategy(protected val app: ScalatraBase) extends ScentryStrategy[LiferayAuthUser] {
+
+  def authenticate()(implicit request: HttpServletRequest, response: HttpServletResponse) = {
+    def findUserAndSetupPermissions = {
+      val user = PortalUtil.getUser(request)
+      val permissionChecker = PermissionCheckerFactoryUtil.create(user)
+
+      PermissionThreadLocal.setPermissionChecker(permissionChecker)
+      PrincipalThreadLocal.setName(user.getUserId)
+      CompanyThreadLocal.setCompanyId(user.getCompanyId)
+
+      LiferayAuthUser(user.getEmailAddress)
+    }
+
+    Try(findUserAndSetupPermissions).toOption
+  }
+
+}
+
+trait AuthenticationSupport extends ScentrySupport[LiferayAuthUser] { self: ScalatraBase =>
+  
+  protected def fromSession = { case id: String => LiferayAuthUser(id)  }
+  protected def toSession   = { case usr: LiferayAuthUser => usr.id }
+
+  override protected def registerAuthStrategies = {
+    scentry.register("LiferayAuth", app => new LiferayAuthStrategy(app))
+  }
+
+  override protected val scentryConfig = new ScentryConfig {
+    override val login = "/c/portal/login"
+    override val returnTo = "/"
+    override val returnToKey = "redirect"
+    override val failureUrl = "/c/portal/logout"
+  }.asInstanceOf[ScentryConfiguration]
+}
--- src/main/scala/com/savoirfairelinux/liferay/portlet/controller/UserController.scala
+++ src/main/scala/com/savoirfairelinux/liferay/portlet/controller/UserController.scala
@@ -1,34 +1,18 @@
 package com.savoirfairelinux.liferay.portlet.controller
 
 import com.liferay.portal.kernel.log.LogFactoryUtil
-import com.liferay.portal.security.auth.CompanyThreadLocal
-import com.liferay.portal.security.auth.PrincipalThreadLocal
-import com.liferay.portal.security.permission.PermissionCheckerFactoryUtil
-import com.liferay.portal.security.permission.PermissionThreadLocal
-import com.liferay.portal.util.PortalUtil
-import com.savoirfairelinux.liferay.portlet.model.User
 import com.savoirfairelinux.liferay.portlet.service.UserService
 import org.json4s.{DefaultFormats, Formats}
 import org.scalatra._
 import org.scalatra.json._
 
-class UserController extends ScalatraServlet with JacksonJsonSupport {
+class UserController extends ScalatraServlet with AuthenticationSupport with JacksonJsonSupport {
   val log = LogFactoryUtil.getLog(getClass())
   protected implicit val jsonFormats: Formats = DefaultFormats
   var userService: UserService = new UserService
 
   before() {
-    try {
-      val user = PortalUtil.getUser(request)
-      val permissionChecker = PermissionCheckerFactoryUtil.create(user)
-
-      PermissionThreadLocal.setPermissionChecker(permissionChecker)
-      PrincipalThreadLocal.setName(user.getUserId)
-      CompanyThreadLocal.setCompanyId(user.getCompanyId)
-    } catch {
-      case ex: Exception =>
-        log.debug("Cannot get users list. Caused by: " + ex.getMessage(), ex)
-    }
+    scentry.authenticate('LiferayAuth)
   }
 
   before("/services/users") {
